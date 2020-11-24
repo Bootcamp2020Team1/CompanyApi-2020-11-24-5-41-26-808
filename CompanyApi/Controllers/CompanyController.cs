87252401588f60aa019f5b7568278a080a6e49d4
Xiaoyu Lu <xlu14@slb.com>
2020-11-24 21:36:32 +0800

    feat: add GetAllCompanies and AddCompany in CompanyController, design test case

---
+++ CompanyApi/Controllers/CompanyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController : ControllerBase
+    {
+        private static IList<Company> companies = new List<Company>();
+        [HttpGet]
+        public IList<Company> Get()
+        {
+            return companies;
+        }
+
+        [HttpPost]
+        public Company AddCompany(Company company)
+        {
+            company.CompanyId = companies.Count + 1;
+            companies.Add(company);
+            return company;
+        }
+    }
+}
---
+++ CompanyApiTest/Controllers/CompanyControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace CompanyApiTest.Controllers
+{
+    public class CompanyControllerTest
+    {
+        private readonly TestServer server = new TestServer(new WebHostBuilder()
+            .UseStartup<Startup>());
+        private readonly HttpClient client;
+
+        public CompanyControllerTest()
+        {
+            client = server.CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_Add_Company_When_Add_Company()
+        {
+            // given
+            Company company = new Company("testCompany");
+            string request = JsonConvert.SerializeObject(company);
+            StringContent requestBody = new StringContent(request, Encoding.UTF8, "application/json");
+
+            // when
+            var response = await client.PostAsync("company", requestBody);
+            var getResponse = await client.GetAsync("company");
+            response.EnsureSuccessStatusCode();
+            var responseString = await getResponse.Content.ReadAsStringAsync();
+            List<Company> actualCompanyList = JsonConvert.DeserializeObject<List<Company>>(responseString);
+            // then
+            Assert.Equal(1, actualCompanyList[0].CompanyId);
+        }
+    }
+}
